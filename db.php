2c2cd706827de248c9ec7b6dbbb784c779e1a93e
keer14102 <99753363+keer14102@users.noreply.github.com>
2023-12-12 09:31:37 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
+$server="gymsys12.mysql.database.azure.com";
+$username="gymsys12";
+$password="Keerthana12#";
 $database="gym";
 
 $conn=mysqli_connect($server,$username,$password,$database);
